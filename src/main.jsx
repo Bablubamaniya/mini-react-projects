974addb80423775d879eb709735ffdaaae0c37bc
Bablu Bamaniya <bamaniyabablu428@gmail.com>
2024-12-21 14:12:42 +0530

    initilis

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,9 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import App from"./App"
+import ReactDOM from "react-dom/client";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const newEle = document.querySelector("#root");
+const root = ReactDOM.createRoot(newEle);
+
+root.render(
+  <App/>
+);
