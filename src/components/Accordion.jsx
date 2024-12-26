692d060805aec0924771862e3c8407fe5db3f4d3
Bablu Bamaniya <bamaniyabablu428@gmail.com>
2024-12-26 18:49:21 +0530

    project done

--- src/components/Accordion.jsx
+++ src/components/Accordion.jsx
@@ -1,34 +1,46 @@
 // import React from 'react'
 import { useState } from "react";
-import "../style/Accordion.module.css"
+import styles from "../style/Accordion.module.css";
+import { HiMiniMinus, HiMiniPlus } from "react-icons/hi2";
 
 function Acordian(props) {
-  const {contents,color} =props;
-  const [open,setOpen] = useState(false);
+  const { contents } = props;
+  const [open, setOpen] = useState(false);
+  function handleClick(index) {
+    setOpen((currState) => (currState === index ? null : index));
+  }
+
   return (
-    <div>
-      {contents.map(function(contents,index){
+    <div className={styles.container}>
+      {contents.map(function (contents, index) {
         return (
-          <Row 
-          title={contents.title}
-          details={contents.details}
-          key={index}
+          <Row
+            title={contents.title}
+            details={contents.details}
+            key={index}
+            handleClick={handleClick}
+            index={index}
+            isOpen={open === index}
           />
         );
       })}
-      </div>
+    </div>
   );
-  function Row(props){
-    const {title,details}=props;
+  function Row(props) {
+    const { title, details, handleClick, index, isOpen } = props;
     return (
-      <div>
-      <h3>{title}</h3>
-      <p>{details}</p>
+      <div className={styles.row}>
+        <h3 onClick={ () => handleClick (index)}  className={styles.title}>
+          <span>
+            {title} {isOpen ? <HiMiniMinus/>:<HiMiniPlus/>}
+            {/* <HiMiniPlus /> */}
+          </span>
+        </h3>
+
+        {isOpen && <p className={styles.details}>{details} </p>}
       </div>
     );
   }
-
 }
-
 
 export default Acordian;
